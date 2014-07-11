241e90f5e237c3cf7735506d2dd3c23d578d8e4c
Aymeric <aymeric.agre@gmail.com>
2014-07-12 00:18:52 +0200

    v07.11

--- mongoosepm/public/javascripts/recherche.js
+++ mongoosepm/public/javascripts/recherche.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){								//Quand la page est chargée
+/* $(document).ready(function(){								//Quand la page est chargée
 	var strHTMLOutput = '';									//Variables listes <li>projet</li> qui accueillera le HTML final
 	$.ajax({'/project/byId/' + projects.id, {					//On appelle le fenêtre ajax '/recherche'
 		dataType : 'json',									//On défini le type comme json
@@ -30,4 +30,38 @@ $(document).ready(function(){								//Quand la page est chargée
 						$('#liste').html(strHTMLOutput);//On met la liste dans l'ul #liste
 					}
 				});
-			});
+			}); */
+
+/*	THEMES	*/						
+function showDetails(theme) {
+	played = $(".play").closest("ul").attr("id");
+	$("input[value="+played+"]").css('background', 'linear-gradient(to bottom, rgba(8,120,120,0.8), rgba(3,60,60,0.8))');	//on remet la couleur normale
+	$(".play").removeClass("play");		//On enlève la classe à ceux qui l'avaient
+	
+	$(theme).css('background','linear-gradient(to bottom, rgba(120,8,8,0.6), rgba(60,3,3,0.8))');	//On identifie la classe sélectionnée par une couleur
+	var category = $(theme).attr("value");	//On récupère son nom
+	$("#"+category+" li").each(function (i) {			//On fait apparaitre les li les uns après les autres
+        $(this).attr("style", "-webkit-animation-delay:" + i * 100 + "ms;"
+                + "-moz-animation-delay:" + i * 100 + "ms;"
+                + "-o-animation-delay:" + i * 100 + "ms;"
+                + "animation-delay:" + i * 100 + "ms;");
+        if (i == $("#"+category+" li").size() -1) {
+			$("#"+category).addClass("play")		//On ajoute la classe "play"	
+        }
+    });
+}
+
+/*	CATEGORY */
+function selectCategory(theme) {
+	if($(theme).attr("class")==="play"){	//S'il était déjà selectionné
+		$(theme).css('background', 'linear-gradient(to bottom, rgba(8,120,120,0.8), rgba(3,60,60,0.8))');	//on remet la couleur normale
+		$(theme).removeClass("play");	//On retire sa classe
+	} else {
+		$(theme).css('background','linear-gradient(to bottom, rgba(120,8,8,0.6), rgba(60,3,3,0.8))');	//On identifie la classe sélectionnée par une couleur
+		$(theme).addClass("play");	//On ajoute la class play
+	}
+}
+	
+	
+	
+	
