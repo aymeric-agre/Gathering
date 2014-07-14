20628620bedd401fa9f0f72dad5461c2cf6aeed5
Aymeric <aymeric.agre@gmail.com>
2014-07-14 05:30:05 +0200

    vérification formulaire inscription
    ajout vérification longueur du password et concordance password avec la
    confirmation, problème descente input lors de l'affichage du message
    pour le password

--- mongoosepm/public/javascripts/identite_formulaire.js
+++ mongoosepm/public/javascripts/identite_formulaire.js
@@ -91,40 +91,82 @@ var tailleFinale = function(origineW, origineH, maxW, maxH){
 	Fonctions de vérification
 	*************************	*/
 
+var allIsOk = 1;	//Variable pour vérifier que le formulaire est conforme
+var mail_verif = 1;	//Variable de vérification du mail (de base 1 pour refuser l'envoi du formulaire)
+var pass_verif = 1;	//Variable de vérification du mot de passe (de base 1 pour refuser l'envoi du formulaire)
+var passLength_verif = 1;	//Variable de vérification de la longueur du mdp (de base 1)
+
 //vérification mail
 $(function(){
 	$('#mail').focusout(function(){
 		var mailExplosed = mail.value.split('@')[1];
 		if(mailExplosed.search(".ec-lyon.") < 0 && mailExplosed.search(".em-lyon.") < 0){
-			document.getElementById('mail_verification').innerHTML = "Cet email n\'est pas valide";
+			document.getElementById('mail_verification').innerHTML = "Cet email n\'est pas valide.";
+			//mail_verif++;	//ceci permet d'éviter que le mail soit valide puis modifié
 		}else{
 			document.getElementById('mail_verification').innerHTML = "";
+			mail_verif = 0;	//le test est un succès, on met la variable de vérification à 0
 		}
 	});
 });
 
-/*
-var fieldalias="mot de passe"
-function verifier_pass(confirm_pass, pass){
-	if(pass.value=''){
-		alert("Veuillez entrer votre " + fieldalias + " dans le premier champ!")
-		pass.focus()
-	}
-	else if(confirm_pass.value==''){
-		alert("Veuillez confirmer votre "+ fieldalias + " dans le second champ!")
-		confirm_pass.focus()
-	}
-	else if
-*/
-/*
-function verifier_pass(confirm_pass,pass)
-{	
-	console.log(confirm_pass + pass);
-	//Par exemple, si tu veux que le pseudo ait entre 4 et 25 caractères
-	if(confirm_pass.value!=pass.value){
-	confirm_pass.style.backgroundColor = 'red';
+//Vérification que le mot de passe soit de longueur suffisante
+function verifPassLength(){
+	var x = password.value.length;
+	var canvas = document.getElementById('passToughnessCanvas');
+	var context = canvas.getContext('2d');
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	var degrade = context.createLinearGradient(0, 0, x*16, x*2);
+	if(x <4){
+		document.getElementById('password_verification').innerHTML = "Mot de passe trop court.";
+		degrade.addColorStop(1, "#c8c8c8");
+		degrade.addColorStop(0, "#ff0000");
+		passLength_verif++;
+	}else if(x >=4 && x <9){
+		document.getElementById('password_verification').innerHTML = "Longueur du mot de passe: faible";
+		degrade.addColorStop(1, "#c8c8c8");
+		degrade.addColorStop(0, "#ffaa00");
+		passLength_verif = 0;
+	}else if(x >=9 && x <13){
+		document.getElementById('password_verification').innerHTML = "Longueur du mot de passe: moyen";
+		degrade.addColorStop(1, "#c8c8c8");
+		degrade.addColorStop(0, "#26ff8a");
+		passLength_verif = 0;
 	}else{
-	confirm_pass.style.backgroudColor = 'none';
+		document.getElementById('password_verification').innerHTML = "Longueur du mot de passe: bon";
+		degrade = "#26ff8a";
+		passLength_verif = 0;
 	}
-}
+	context.fillStyle = degrade;
+	context.fillRect(0, 0, x*18, 300);
+};
+
+//vérification mot de passe identique à la confirmation
+$(function(){
+	$('#confirm_password').blur(function(){
+		if(password.value !== confirm_password.value){
+			document.getElementById('confirmedPassword_verification').innerHTML = "Le mot de passe n\'est pas identique.";
+			pass_verif++;	//ceci permet d'éviter que le password soit valide puis modifié
+		}else{
+			document.getElementById('confirmedPassword_verification').innerHTML = "";
+			pass_verif = 0;	//le test est un succès, on met la variable de vérification à 0
+		}
+	});
+});
+/*
+//vérification que les champs de données personnelles sont bien remplis
+function verifForm(){
+	if(mail.value == '' || password.value == '' || confirm_password.value == '' || userLastName.value == '' || userFirstName.value == '' || birthDate.value == '' || country.value == '' || area.value == '' || town.value == '' || phone.value == ''){
+		alert('Veuillez remplir toutes les informations personnelles.');
+		allIsOk = 1;
+	}else if(mail_verif = 1){
+		alert('Veuillez vérifier votre mail.');
+		allIsOk = 1;
+	}else if(pass_verif = 1 || passLength_verif = 1){
+		alert('Veuillez vérifier votre mot de passe.');
+		allIsOk = 1;
+	}else{
+		return (allIsOk ==0);	//Retourne true si tous les tests sont ok
+	}
+};
 */
