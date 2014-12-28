5206f6d07628b93d5e69cd9eb27a19aa8522abb3
Jean2bob <francois.guezengar@hotmail.fr>
2014-12-28 19:40:36 +0100

    séparation user / connectedUser -> fonctionnelle

--- mongoosepm/public/controllers/projects_list_controller.js
+++
@@ -1,6 +0,0 @@
-'user strict';	//Permet une certaine optimisation de l'interprétation du code (et rend les erreurs javascript non sillencieuses)
-
-
-gatheringModule.controller('projectsListController', ['$rootScope', '$state',  function($scope, $state) {
-
-}]);
