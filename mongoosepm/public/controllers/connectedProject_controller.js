2a3b1704eb96f8f4e4cbba921d630f64e61a2e13
Jean2bob <francois.guezengar@hotmail.fr>
2014-12-30 23:16:45 +0100

    Update fonctionne ....
    Mais c'est pas très propre : il faut transformer chaque élément
    précédemment peuplé en id... Ca va tant qu'il n'y en a que trois, mais à
    un moment ça deviendra vraiement compliqué

--- mongoosepm/public/controllers/connectedProject_controller.js
+++ mongoosepm/public/controllers/connectedProject_controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*	Controller de la liste des projet : hérite de profile	*/
-gatheringModule.controller('connectedProjectController', ['$scope','thisProject', 'Project', function($scope, thisProject, Project){	//project désigne le projet de la page
+gatheringModule.controller('connectedProjectController', ['$scope','thisProject', 'Project', '$state', function($scope, thisProject, Project, $state){	//project désigne le projet de la page
 	$scope.project = thisProject;
 
 /*	****
@@ -10,6 +10,17 @@ gatheringModule.controller('connectedProjectController', ['$scope','thisProject'
 	
 $scope.updateProject = function(){
 	var projectToUpdate = new Project({project : $scope.project});
+	var j,i, k;
+	for(j = 0; j<projectToUpdate.project.public.themes.length; j++){
+		projectToUpdate.project.public.themes[j] = projectToUpdate.project.public.themes[j]._id;	//On remplace le populate par son id
+	};
+	for(i = 0; i<projectToUpdate.project.public.competences.length; i++){
+		projectToUpdate.project.public.competences[i] = projectToUpdate.project.public.competences[i]._id;	//On remplace le populate par son id
+	};
+	
+	for(k = 0; k<projectToUpdate.project.public.members.length; k++){
+		projectToUpdate.project.public.members[k] = projectToUpdate.project.public.members[k]._id;	//On remplace le populate par son id
+	};
 	console.log(projectToUpdate);	//PROBLEME : Il y a une partie resolve ...
 	projectToUpdate.$update({id : $scope.project._id}, function(projectUpdated){
 		$state.go('main.connectedProject.presentation', ({projectId : projectUpdated._id}));
